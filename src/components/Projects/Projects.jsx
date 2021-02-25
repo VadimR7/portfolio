c8e24daf88a2b88b58467a7efe30de33ac3c6af7
VadimR <roibu.vadim@gmail.com>
2021-02-25 12:11:26 +0200

    about me edit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -57,7 +57,6 @@ const Projects = () => {
                       >
                         See Live
                       </a>
-
                       {repo && (
                         <a
                           target="_blank"
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,25 +2,28 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Vadim Roibu Portfolio', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello, my name is',
+  name: 'Vadim',
+  subtitle: 'I am a Junior QA tester and Web Developer',
+  cta: 'more About me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne:
+    'My name is Vadim. I live in Moldova, Chișinău. I am just starting my career in IT.',
+  paragraphTwo:
+    'I began my path with a QA course in Software Testing. For now, I am improving my skills on freelance platforms like utes.com and test.io. My focus is more on Web Applications and, I think that to be a better Web application QA engineer, you have to know very well how the system is working. That`s why I am actively learning Web Development and Advance JavaScript Programming on Udemy.',
+  paragraphThree:
+    'For the future, I want to learn Phyton or Java and at least one test automation framework. I think that will help me to achieve my final goal - to become a QA Automation Engineer.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -29,11 +32,11 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
+    title: 'WordPress Websites',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://metalprim.md',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
